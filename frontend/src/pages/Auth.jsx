2935d306ce12a5021f6adf30a741aa929f302840
Sumit240803 <goyalsumit651@gmail.com>
2025-01-05 13:31:08 +0530

    Development completed

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import './auth.css';
+import './Auth.css';
 
 const Auth = () => {
   const [isRegister, setIsRegister] = useState(false);
